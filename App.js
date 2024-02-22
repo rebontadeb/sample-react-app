c9a6a34b924f118e3f1faa19c72097a024108cc5
Rebonta <rebontadeb@gmail.com>
2024-02-22 13:55:01 +0530

    openshift_s2i

---
+++ App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My OpenShift React App</h1>
+        <p>
+          This is a basic React.js application deployed on OpenShift.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
